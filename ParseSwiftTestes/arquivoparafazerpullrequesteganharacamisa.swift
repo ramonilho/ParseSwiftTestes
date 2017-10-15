2e0c3539d07d93f61928d625d5669969a4590283
Humberto <humberto.castro@mira.vc>
2017-10-15 14:23:12 -0200

    commit apenas pra ganhar a camisa

---
+++ ParseSwiftTestes/arquivoparafazerpullrequesteganharacamisa.swift
@@ -0,0 +1,9 @@
+//
+//  arquivoparafazerpullrequesteganharacamisa.swift
+//  ParseSwiftTestes
+//
+//  Created by Humberto Vieira on 15/10/17.
+//  Copyright © 2017 Ramon. All rights reserved.
+//
+
+import Foundation
